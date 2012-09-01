1c7f1e67585c4fe0d63ea3f67380259e4193dedf
mabarroso <mabarroso@mabarroso.com>
2012-09-01 23:59:18 +0200

    The board

---
+++ lib/board.rb
@@ -0,0 +1,107 @@
+class Board
+
+  $board_id = 0
+
+  attr_accessor :name
+
+  def initialize name = false
+    $board_id   += 1
+    @id         = $board_id
+    @name       = name ? name : 'unnamed' + $board_id.to_s
+    @last_uid   = -1
+    @columns      = {}
+    @current    = 0
+  end
+
+  def size
+    @columns.count
+  end
+
+  def empty?
+    @columns.count == 0
+  end
+
+  def add column
+    @columns[next_uid]  = column
+  end
+
+  alias :<< :add
+
+  def exists? column
+    find_uid column
+  end
+
+  def delete column_to_delete
+    uid = find_uid column_to_delete
+    @columns.delete uid if uid
+    uid
+  end
+
+  def each &blk
+    return unless block_given?
+    @columns.keys.each{|uid| yield uid, @columns[uid] }
+  end
+
+  def first
+    @current = 0
+  end
+
+  def last
+    @current = size - 1
+  end
+
+  def next
+    @current += 1
+    if @current > size - 1
+      @current = size - 1
+      return false
+    end
+    return true
+  end
+
+  def next?
+  	@current < size - 1
+	end
+
+  def previous
+    @current -= 1
+    if @current < 0
+      @current = 0
+      return false
+    end
+    return true
+  end
+
+  def previous?
+  	@current > 0
+	end
+
+  def go position
+    @current = position if position < size
+  end
+
+  def current
+    @current
+	end
+
+  def column
+    @columns[@columns.keys[@current]]
+	end
+
+  private
+  def uid
+    @last_uid
+  end
+
+  def next_uid
+    @last_uid += 1
+  end
+
+  def find_uid column_to_find
+    each do |uid, column|
+      return uid if column_to_find.equal? column
+    end
+    false
+  end
+
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,168 @@
+root = File.expand_path('../../lib', __FILE__)
+require File.join(root, 'column')
+require File.join(root, 'board')
+
+describe Board do
+  BOARD_LAST = 6
+
+  def filled
+    board = Board.new
+    BOARD_LAST.times do |i|
+      column = Column.new (i+1).to_s
+      board << column
+    end
+    board
+  end
+
+  it "should be empty" do
+  	d = Board.new
+  	d.empty?.should be_true
+  end
+
+  it "default names should be different" do
+  	d1 = Board.new
+  	d2 = Board.new
+  	d1.name.should_not == d2.name
+  end
+
+  it "should add" do
+    d = Board.new
+    size = d.size
+    column = Column.new
+    d << column
+		d.size.should == size + 1
+  end
+
+  it "should be first equal 1" do
+  	d = filled
+		d.first
+		d.column.name.should == '1'
+  end
+
+  it "should be last equal {BOARD_LAST}" do
+  	d = filled
+		d.last
+		d.column.name.should == BOARD_LAST.to_s
+  end
+
+  it "should be last-1 equal {BOARD_LAST-1}" do
+  	d = filled
+		d.last
+		d.previous
+		d.column.name.should == (BOARD_LAST-1).to_s
+  end
+
+  it "should be go to first position equal first" do
+  	d = filled
+  	d.first
+  	position = d.current
+  	d.last
+  	d.go position
+  	d.current.should == position
+  end
+
+  it "should be go to last position equal last" do
+  	d = filled
+  	d.last
+  	position = d.current
+  	d.first
+  	d.go position
+  	d.current.should == position
+  end
+
+  it "should be false when next from last" do
+  	d = filled
+  	d.last
+  	d.next.should be_false
+  end
+
+  it "should be false when previous from last" do
+  	d = filled
+  	d.first
+  	d.previous.should be_false
+  end
+
+  it "should be last when next from last" do
+  	d = filled
+  	d.last
+  	d.next
+    d.current.should == d.last
+  end
+
+  it "should be first when previous from first" do
+  	d = filled
+  	d.first
+  	d.previous
+    d.current.should == d.first
+  end
+
+  it "should be no next from last" do
+  	d = filled
+		d.last
+		d.next?.should be_false
+  end
+
+  it "should be no previous from first" do
+  	d = filled
+		d.first
+		d.previous?.should be_false
+  end
+
+  it "should be next from first" do
+  	d = filled
+		d.first
+		d.next?.should be_true
+  end
+
+  it "should be previous from last" do
+  	d = filled
+		d.last
+		d.previous?.should be_true
+  end
+
+  it "should be next from previous of last" do
+  	d = filled
+		d.last
+		d.previous
+		d.next?.should be_true
+  end
+
+  it "should be previous from next of first" do
+  	d = filled
+		d.first
+		d.next
+		d.previous?.should be_true
+  end
+
+  it "should not exists" do
+  	d = filled
+    column = Column.new (BOARD_LAST+1).to_s
+		d.exists?(column).should be_false
+  end
+
+  it "should exists" do
+  	d = filled
+    column = Column.new (BOARD_LAST+1).to_s
+    d << column
+		d.exists?(column).should be_true
+  end
+
+
+  it "should not delete inexistent column" do
+  	d = filled
+    column = Column.new (BOARD_LAST+1).to_s
+    size = d.size
+		d.delete column
+		d.size.should == size
+  end
+
+  it "should delete column" do
+  	d = filled
+    column = Column.new (BOARD_LAST+1).to_s
+    d << column
+    size = d.size
+		d.delete column
+		d.size.should == size - 1
+  end
+
+end
