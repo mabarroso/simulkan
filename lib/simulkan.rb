8e99256e7495d6c894383823d427a816d3c605b7
mabarroso <mabarroso@mabarroso.com>
2012-09-05 00:34:40 +0200

    Define board columns

--- lib/simulkan.rb
+++ lib/simulkan.rb
@@ -1,15 +1,21 @@
 root = File.expand_path('../../lib/simulkan', __FILE__)
 require File.join(root, 'card')
-require File.join(root, 'resoure')
+require File.join(root, 'resource')
 require File.join(root, 'column')
 require File.join(root, 'board')
 
 
 backlog = Column.new 'Backlog'
 historylog = Column.new 'Historylog'
+
 board = Board.new
+board << backlog
+board << Column.new('Design', 3, 3)
+board << Column.new('Development', 3, 3)
+board << Column.new('QA', 3, 3)
+board << historylog
 
-board << Column.new 'Design', 3, 3
-board << Column.new 'Development', 3, 3
-board << Column.new 'QA', 3, 3
-
+20.times do |i|
+  card = Card.new (i+1).to_s
+  backlog << card
+end
