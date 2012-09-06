4547b9fe6f6099c7c85104a0dea58754c2b6afd7
mabarroso <mabarroso@mabarroso.com>
2012-09-07 01:22:57 +0200

    card story points by column

--- lib/simulkan/card.rb
+++ lib/simulkan/card.rb
@@ -1,15 +1,43 @@
 class Card
 
+	STORY_POINTS = 5
+
   $card_id = 0
 
   attr_reader :id
-  attr_accessor :name, :body
+  attr_accessor :name, :body, :columns_points
 
-  def initialize name = false, body={}
-    $card_id   += 1
-    @id         = $card_id
-    @name       = name || $card_id.to_s
-    @body       = body
+  def initialize name = false, opts = {}
+    $card_id       += 1
+    @id             = $card_id
+    @name           = name || $card_id.to_s
+    @body           = opts[:body] || ''
+    set_column_points opts[:columns_points] || 0
+  end
+
+  def set_column_points columns_points
+    @columns_points	= []
+    if columns_points.is_a? Array
+    	columns_points.each do |points|
+    		@columns_points << points
+    	end
+    else
+      columns_points.times do |i|
+      	@columns_points << STORY_POINTS
+      end
+  	end
+  end
+
+  def get_column_points column
+  	@columns_points[column]
+  end
+
+	#
+  def consume_column_points column, points
+  	# return positive -> completed and more points
+  	# return 0 -> completed and no more points
+  	# return negative -> o completed and no more points
+  	points - @columns_points[column]
   end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -13,5 +13,34 @@ describe Card do
   	c1.name.should == c1.id.to_s
   end
 
+  it "all column point must be equal" do
+  	c = Card.new
+  	c.set_column_points 2
+  	c.get_column_points(0).should == c.get_column_points(1)
+  end
+
+  it "all column point must be different" do
+  	c = Card.new
+  	c.set_column_points [1,2]
+  	c.get_column_points(0).should_not == c.get_column_points(1)
+  end
+
+  it "consume_column_points must be 0" do
+  	c = Card.new
+  	c.set_column_points [5]
+  	c.consume_column_points(0, 5).should == 0
+  end
+
+  it "consume_column_points must be positive" do
+  	c = Card.new
+  	c.set_column_points [5]
+  	c.consume_column_points(0, 6).should > 0
+  end
+
+  it "consume_column_points must be negative" do
+  	c = Card.new
+  	c.set_column_points [5]
+  	c.consume_column_points(0, 4).should < 0
+  end
 
 end
