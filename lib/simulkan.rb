8e4e35b1db2e89a60110a2c227cf48121ef0131b
mabarroso <mabarroso@mabarroso.com>
2012-09-26 14:50:10 +0200

    Tuning wip and resources

--- lib/simulkan.rb
+++ lib/simulkan.rb
@@ -8,8 +8,8 @@ require File.join(root, 'column')
 require File.join(root, 'board')
 require File.join(root, 'exceptions/wip_exception')
 
-UNCERTAINTY = true
-RESOURCE_POINTS = 9
+UNCERTAINTY = false
+RESOURCE_POINTS = 6
 
 def snapshot board
 	r = ''
@@ -93,10 +93,10 @@ def graph_sla board
   axis_x_days_percent = Hash[axis_x_days.map {|k,v| [k, (v==0)?'':"#{100*v/total_work}%"]}]
   axis_x_days_sla = Hash[axis_x_days.map {|k,v| sum = axis_x_days.keys.inject{|sum,i| (i>k)?sum:sum + axis_x_days[i] }; [k, "#{100*sum/total_work}%"]}]
 
-  puts axis_x_days.keys.to_s
-  puts axis_x_days_sla.values.to_s
-  puts axis_x_days.values.to_s
-  puts '---'
+#  puts axis_x_days.keys.to_s
+#  puts axis_x_days_sla.values.to_s
+#  puts axis_x_days.values.to_s
+#  puts '---'
 
 	Gchart.bar( data: axis_x_days.values,
 							axis_with_labels: ['x', 'x', 'x', 'x', 'y'],
@@ -112,7 +112,7 @@ board = Board.new
 board << backlog
 board << Column.new('Design', {wip: 3, resources_hight: 2, resource_points: RESOURCE_POINTS, uncertainty: UNCERTAINTY})
 board << Column.new('Development', {wip: 5, resources_hight: 3, resource_points: RESOURCE_POINTS, uncertainty: UNCERTAINTY})
-board << Column.new('QA', {wip: 2, resources_hight: 1, resource_points: RESOURCE_POINTS, uncertainty: UNCERTAINTY})
+board << Column.new('QA', {wip: 3, resources_hight: 2, resource_points: RESOURCE_POINTS, uncertainty: UNCERTAINTY})
 board << historylog
 
 CYCLES = 30
@@ -147,10 +147,3 @@ puts log
 puts graph_acumulative graph, board
 puts
 puts graph_sla board
-
-board.first
-board.each do |column|
-	column.each do |card|
-#	  puts "->##{card.id} Class: #{card.service_class} #{card.history}\n"
-	end
-end
