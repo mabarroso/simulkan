c0f6b1f35dab6e2386ec740976ade0413dc73eff
mabarroso <mabarroso@mabarroso.com>
2012-09-28 00:03:23 +0200

    Aceptance tests

--- spec/scenarios/dummy_spec.rb
+++ spec/scenarios/dummy_spec.rb
@@ -2,4 +2,8 @@ root = File.expand_path('../../../lib/simulkan', __FILE__)
 require File.join(root, 'scenarios/dummy')
 
 describe Simulkan::Dummy do
+  it "should be correct class" do
+    s = Simulkan.use :dummy
+    s.name.should ==  Simulkan::Dummy
+  end
 end
--- spec/scenarios/factory_spec.rb
+++ spec/scenarios/factory_spec.rb
@@ -2,7 +2,7 @@ root = File.expand_path('../../../lib/simulkan', __FILE__)
 require File.join(root, 'scenarios/factory')
 
 describe Simulkan::Factory do
-  it "should be correct size" do
+  it "should be correct class" do
     s = Simulkan.use :dummy
     s.name.should ==  Simulkan::Dummy
   end
