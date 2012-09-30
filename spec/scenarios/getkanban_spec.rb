8f53849ccc5333f250e19e350b06e8d534aab872
mabarroso <mabarroso@mabarroso.com>
2012-09-30 23:50:18 +0200

    Aceptance tests

--- spec/scenarios/getkanban_spec.rb
+++ spec/scenarios/getkanban_spec.rb
@@ -2,4 +2,8 @@ root = File.expand_path('../../../lib/simulkan', __FILE__)
 require File.join(root, 'scenarios/getkanban')
 
 describe Simulkan::Getkanban do
+  it "should be correct class" do
+    s = Simulkan.use :getkanban
+    s.name.should ==  Simulkan::Getkanban
+  end
 end
