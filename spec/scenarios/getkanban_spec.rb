c05677bedc14a6566d638aca5931204e5e711ede
mabarroso <mabarroso@mabarroso.com>
2012-10-03 21:52:45 +0200

    Check for card service types

--- spec/scenarios/getkanban_spec.rb
+++ spec/scenarios/getkanban_spec.rb
@@ -7,9 +7,22 @@ describe Simulkan::Getkanban do
     s.name.should ==  Simulkan::Getkanban
   end
 
-  it "should be correct number of cardsclass" do
+  it "should be correct number of cards" do
     s = Simulkan.use :getkanban
     s.cards.count.should ==  36
+  end
+
+  it "should be correct number of cards for class" do
+    s = Simulkan.use :getkanban
+    c = {}
+    s.cards.each do |card|
+      c[card.service_class] = c[card.service_class] ? c[card.service_class] + 1 : 1
+    end
+    c.keys.count.should == 4
+    c[Card::CLASS_NORMAL].should ==  30
+    c[Card::CLASS_EXPEDITE].should ==  1
+    c[Card::CLASS_FIXDATE].should ==  2
+    c[Card::CLASS_INTANGIBLE].should ==  3
   end
 
 end
