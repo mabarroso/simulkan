405deab80006ffb780e50e1cf16f20d95c365ad7
mabarroso <mabarroso@mabarroso.com>
2012-10-06 23:17:34 +0200

    SLA graph

--- lib/simulkan/graph.rb
+++ lib/simulkan/graph.rb
@@ -34,4 +34,40 @@ class Graph
   							size: '600x300'
   							)
   end
+
+  def self.graph_sla board
+  	axis_x_days = {}
+  	last_column = board.size - 1
+  	board.first
+  	board.each do |column|
+  		column.each do |card|
+  			if card.history[last_column]
+  				days = card.history[last_column] - card.history[1]
+  				axis_x_days[days] = 0 if axis_x_days[days] == nil
+  				axis_x_days[days] += 1
+  			end
+  #		  puts "->##{card.id} Class: #{card.service_class} #{card.history}\n"
+  		end
+  	end
+  	axis_x_days.keys.max.times do |i|
+  		axis_x_days[i] = 0 if axis_x_days[i] == nil
+  	end
+  	sorted_array = axis_x_days.sort
+  	axis_x_days = Hash[sorted_array.map {|k,v| [k,v]}]
+  	total_work = axis_x_days.values.inject{|sum,x| sum + x }
+    axis_x_days_percent = Hash[axis_x_days.map {|k,v| [k, (v==0)?'':"#{100*v/total_work}%"]}]
+    axis_x_days_sla = Hash[axis_x_days.map {|k,v| sum = axis_x_days.keys.inject{|sum,i| (i>k)?sum:sum + axis_x_days[i] }; [k, "#{100*sum/total_work}%"]}]
+
+  #  puts axis_x_days.keys.to_s
+  #  puts axis_x_days_sla.values.to_s
+  #  puts axis_x_days.values.to_s
+  #  puts '---'
+
+  	Gchart.bar( data: axis_x_days.values,
+  							axis_with_labels: ['x', 'x', 'x', 'x', 'y'],
+             		axis_labels: [axis_x_days.keys, axis_x_days_sla.values, axis_x_days.values, axis_x_days_percent.values, 1.step(axis_x_days.values.max,1).to_a],
+             		size: '600x300'
+             	)
+  end
+
 end
